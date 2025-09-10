5ca7f7c721ab229531d443d71d361b4eb988048b
Aravind <gmarav005@gmail.com>
2025-09-10 19:51:21 +0530

    atleast working

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
 const container = document.querySelector("#container");
+const button = document.querySelector('button');
 
-const row = document.createElement("div");
-row.classList.add("row");
-row.textContent = " ";
-container.appendChild(row);
+let input = 16;
 
+button.addEventListener("click", () => {
+   let userInput = prompt("Grid size");
+   createGrid(input);
+        input = parseInt(userInput);
+
+        if (input > 100) {
+          alert("Grid should be less than 100");
+        } else {
+               createGrid(input);
+        }
+
+});
+
+createGrid(16);
+
+
+function createGrid(input) {
+
+        for (let index = 1; index <=input; index++) {
+
+            const row = document.createElement("div");
+            row.classList.add("row");
+            row.textContent = " ";
+
+            for (let index = 1; index <=input; index++) {
+
+                const column = document.createElement("div");
+                column.classList.add("column");
+                column.textContent = " ";
+                column.addEventListener('mouseenter', () => {
+                    column.style.backgroundColor = 'blue'
+                });
+                container.appendChild(column);
+
+            }
+
+            row.addEventListener('mouseenter', () => {
+                row.style.backgroundColor = 'blue'
+            });
+
+            container.appendChild(row);
+
+        }   
+}
